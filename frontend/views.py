9549b05d36eda63702d3410bf75c62ad5396f8b5
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-02 12:20:22 +0600

    In the frontend, all we have is written simple views

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# frontend/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic.detail import DetailView
+
+from todos.models import Todo
+
+
+def index(request):
+    return render(request, 'frontend/index.html')
+
+
+class TodoDetailView(DetailView):
+    model = Todo
+    template_name = 'frontend/index.html'
