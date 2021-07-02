187a556a427816f8d9c560ce4586262cb9a71a53
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-02 20:33:14 +0600

    index.js created and class based component App.js created

---
+++ frontend/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    render() {
+        return (
+
+            <div>
+                <h1>TODO CRUD</h1>
+            </div>
+
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
