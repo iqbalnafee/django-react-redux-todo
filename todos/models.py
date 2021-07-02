ca609ddd3c81c28591fdf3e5bb76e3fb2911e9a3
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-02 11:05:56 +0600

    task model created

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):  # model name should be start with capital letter
+    task = models.CharField(max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task
