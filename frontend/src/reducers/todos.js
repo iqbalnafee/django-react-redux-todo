17ad36c088abdf97bdeb32c4628de7c25924f2a9
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-03 18:32:52 +0600

    todo reducer created

--- frontend/src/reducers/todos.js
+++ frontend/src/reducers/todos.js
@@ -1,3 +1,37 @@
 import _ from lodash;
 
 import { GET_TODOS } from "../actions/types";
+
+export default ( state = {}, action ) => {
+
+
+    const {type,payload} = action;
+
+    switch ( type ) {
+
+        case GET_TODOS: 
+            return{
+
+                ...state,
+                ..._.mapKeys(payload,'id')
+            };
+        default:
+            return state;
+
+    }
+
+}
+
+// const _ = require("lodash");
+  
+// // Using the _.mapKeys() method 
+// console.log(
+//   _.mapKeys({ 'cpp': 15, 'java': 40, 'python': 63 },
+//       function(value, key) {
+//           return  key + value ;
+//   }
+// ));
+
+// Output:
+
+// {'cpp15': 15, 'java40': 40, 'python63': 63}
