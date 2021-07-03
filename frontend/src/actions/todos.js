a02557f5ea5b3ef81f645a071d116f7812e690eb
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-03 12:42:20 +0600

    action todos.js added
    First we create a action todos.js where we get all the todos from server and dispatch it to the reducer

---
+++ frontend/src/actions/todos.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+import {GET_TODOS} from 'types';
+
+export const getTodos = () => async dispatch => {
+
+    const res = await axios.get('/api/todos/');
+
+    dispatch({
+
+        type: GET_TODOS,
+        payload:res.data
+
+    });
+
+}
